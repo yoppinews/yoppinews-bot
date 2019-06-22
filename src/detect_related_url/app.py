807809fc16c1436859b8aa8f094b08acde18d9d3
gomi_ningen <gomi2ngen@gmail.com>
2019-06-23 05:32:07 +0900

    関連 URL 検出時に 短縮 URL 展開を行う

--- src/detect_related_url/app.py
+++ src/detect_related_url/app.py
@@ -13,7 +13,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions
 
 from message import DetectRelatedURLMessage
-from css_selectors import CSSSelector
+from css_selectors import Selectors
 from news_bot_config import NewsBotConfig
 
 stage = os.environ['Stage']
@@ -43,18 +43,21 @@ def lambda_handler(event, _):
         options.binary_location = "/opt/bin/headless-chromium"
         web_driver = Chrome(executable_path="/opt/bin/chromedriver", chrome_options=options)
     message = DetectRelatedURLMessage.of(event)
-    s = CSSSelector(config.detect_url_selectors, config.detect_url_ignored_urls)
-    selector = s.get_selector(message.url)
-    return handle(message, selector, web_driver, config)
+    s = Selectors(config.detect_url_selectors, config.detect_url_ignored_urls)
+    return handle(message, s, web_driver, config)
 
 
-def handle(message: DetectRelatedURLMessage, selector: str, driver: Chrome, config: NewsBotConfig):
+def handle(message: DetectRelatedURLMessage, selectors: Selectors, driver: Chrome, config: NewsBotConfig):
     url = message.url
     logger = config.logger
-    text = get_selected_text(url, selector, driver, logger)
-    detected_text = config.keyword_detector.find_related_keyword(text)
+    res = get_selected_text(url, selectors, driver, logger)
+    expanded_url = res.get('url', None) if res is not None else None
+    selector = res.get('selector', 'body') if res is not None else None
+    selected_text = res.get('selected_text', '') if res is not None else None
+    detected_text = config.keyword_detector.find_related_keyword(selected_text)
     res = {
         'url': url,
+        'expanded_url': expanded_url,
         'selector': selector,
         'detected_text': detected_text,
     }
@@ -62,16 +65,26 @@ def handle(message: DetectRelatedURLMessage, selector: str, driver: Chrome, conf
     return res
 
 
-def get_selected_text(url: str, selector: str, driver: Chrome, logger: Logger) -> Optional[str]:
+def get_selected_text(url: str, selectors: Selectors, driver: Chrome, logger: Logger) -> Optional[dict]:
+    current_url = None
+    selector = None
     try:
         driver.get(url)
-        WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, selector)))
-        return driver.find_element_by_css_selector(selector).text
+        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, 'body')))
+        current_url = driver.current_url
+        selector = selectors.get_selector(current_url)
+        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, selector)))
+        selected_text = driver.find_element_by_css_selector(selector).text
+        return {
+            'url': current_url,
+            'selector': selector,
+            'selected_text': selected_text,
+        }
     except TimeoutException as e:
         logger.error(json.dumps({
             'event': 'detect_related_url:get_selected_text:timeout',
             'details': {
-                'url': url,
+                'url': current_url or url,
                 'selector': selector,
                 'error': e.__str__()
             }
