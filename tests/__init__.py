d932d3f059e1192cdf566a0ab20a2c6b1225d09b
gomi_ningen <gomi2ngen@gmail.com>
2019-07-03 04:29:16 +0900

    update

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,3 @@ import os
 os.environ['Stage'] = 'local'
 os.environ['ConfigBucket'] = 'news-bot'
 os.environ['ConfigKeyName'] = 'config.json'
-os.environ['AWS_ACCESS_KEY_ID'] = '................'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '................'
